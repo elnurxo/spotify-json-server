040a27c90ab436059b593a8115e7bc36fe821e40
Elnur Khalilov <87971131+elnurxo@users.noreply.github.com>
2024-07-27 14:18:32 +0400

    Update server.js

--- api/server.js
+++ api/server.js
@@ -1,26 +1,15 @@
-// See https://github.com/typicode/json-server#module
-const jsonServer = require('json-server')
+const jsonServer = require('json-server');
+const path = require('path');
+const fs = require('fs');
 
-const server = jsonServer.create()
+module.exports = (req, res) => {
+  const server = jsonServer.create();
+  const router = jsonServer.router(path.join(__dirname, '../public/db.json'));
+  const middlewares = jsonServer.defaults();
 
-// Uncomment to allow write operations
-const fs = require('fs')
-const path = require('path')
-const filePath = path.join('db.json')
-const data = fs.readFileSync(filePath, "utf-8");
-const db = JSON.parse(data);
-const router = jsonServer.router(db)
+  server.use(middlewares);
+  server.use(router);
 
-// Comment out to allow write operations
-//const router = jsonServer.router('db.json')
-
-const middlewares = jsonServer.defaults()
-
-server.use(router)
-server.use(middlewares)
-server.listen(3000, () => {
-    console.log('JSON Server is running')
-})
-
-// Export the Server API
-module.exports = server
+  // Start the server
+  server(req, res);
+};
